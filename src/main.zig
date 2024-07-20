0e56484aa840fac6d74e4810010a0f2be406379d
Jonathan Anbary <jonathananbary@gmail.com>
2024-07-20 12:44:39 +0300

    Initial commit

---
+++ src/main.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+const libelf = @cImport(@cInclude("libelf.h"));
+
+pub fn main() !void {
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    try stdout.print("libelf.Elf_Cmd.ELF_C_READ = {}", .{libelf.Elf_Cmd.ELF_C_READ});
+
+    try bw.flush(); // don't forget to flush!
+}
+
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
